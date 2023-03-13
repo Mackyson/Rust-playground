8b0c8b4c0308cb4119ecd5c10f8d2ad360e17923
Mackyson <27495193+Mackyson@users.noreply.github.com>
2023-03-13 22:58:16 +0900

    number guessing game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,25 +1,48 @@
-use std::{io, num::ParseIntError};
+use rand::Rng;
+use std::{cmp::Ordering, io, num::ParseIntError};
 
 fn main() {
-    let _maximum_try = 5;
+    let maximum_try = 7;
+    let mut num_try = 0;
+    let secret_number = get_rand();
+
     println!("Guess the number!");
 
-    println!("Input your guess.");
-
-    let input = input();
-
-    match input {
-        Ok(guess) => {
-            println!("you guessed the number is {}", guess);
+    loop {
+        if num_try >= maximum_try {
+            println!("GAME OVER");
+            return;
         }
-        Err(_e) => {
-            println!("It's not a number.");
+
+        println!("Input your guess.");
+
+        let input = parse_input();
+
+        match input {
+            Err(_e) => {
+                println!("It's not a number.");
+                continue;
+            }
+            Ok(guess) => match guess.cmp(&secret_number) {
+                Ordering::Less => println!("secret number is greater than {}", guess),
+                Ordering::Greater => println!("secret number is lesser than {}", guess),
+                Ordering::Equal => {
+                    println!("secret number is {}! Congratulations!", guess);
+                    return;
+                }
+            },
         }
+        num_try += 1;
     }
 }
-fn input() -> Result<i32, ParseIntError> {
+
+fn parse_input() -> Result<i32, ParseIntError> {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read.");
 
-    input.strip_suffix("\n").unwrap().parse::<i32>()
+    input.trim().parse::<i32>()
+}
+
+fn get_rand() -> i32 {
+    rand::thread_rng().gen_range(1..100)
 }
